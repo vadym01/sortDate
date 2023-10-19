d9c6b9c55710234b6a2a1b3f2f27f0da314d3a81
vadym <vasylaki95@gmail.com>
2023-10-19 11:20:25 +0300

    date sorter solution

---
+++ DateSorter.java
@@ -0,0 +1,52 @@
+package saple;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Marking will be based upon producing a readable, well engineered solution rather than factors
+ * such as speed of processing or other performance-based optimizations, which are less
+ * important.
+ *
+ *
+ package sample;
+
+ import java.time.LocalDate;
+ import java.util.Collection;
+ import java.util.List;
+
+ /**
+ * Marking will be based upon producing a readable, well engineered solution rather than factors
+ * such as speed of processing or other performance-based optimizations, which are less
+ * important.
+ *
+ * Implement in single class. Don't chane constructor and signature method.
+ */
+public class DateSorter {
+
+    /**
+     * The implementation of this method should sort dates.
+     * The output should be in the following order:
+     * Dates with an 'r' in the month,
+     * sorted ascending (first to last),
+     * then dates without an 'r' in the month,
+     * sorted descending (last to first).
+     * For example, October dates would come before May dates,
+     * because October has an 'r' in it.
+     * thus: (2004-07-01, 2005-01-02, 2007-01-01, 2032-05-03)
+     * would sort to
+     * (2005-01-02, 2007-01-01, 2032-05-03, 2004-07-01)
+     *
+     * @param unsortedDates - an unsorted list of dates
+     * @return the collection of dates now sorted as per the spec
+     */
+    public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
+        Stream<LocalDate> sortedAsc = unsortedDates.stream().filter(m -> m.getMonth().name().contains("R")).sorted();
+        Stream<LocalDate> sortedDesc = unsortedDates.stream().filter(m -> !m.getMonth().name().contains("R")).sorted(Comparator.reverseOrder());
+        return Stream.concat(sortedAsc, sortedDesc).collect(Collectors.toList());
+    }
+}
